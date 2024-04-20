4a3a476a463bd3d84aa477c1abe86b04838f60e8
Jayde1600 <neomeslane@gmail.com>
2024-04-20 12:04:46 +0200

    I finished services section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const header = document.querySelector("header");
 
 window.addEventListener ("scroll", function() {
-    header.classList.toggle("sticky", window.scrollY > 200)
+    header.classList.toggle ("sticky", window.scrollY > 200);
 });
